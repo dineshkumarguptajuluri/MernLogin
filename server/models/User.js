519851ee910d89f5037ba7be5fbfa56fa6a87e3b
Dinesh Kumar Gupta Juluri <juluridineshkumar@gmail.com>
2024-02-18 22:04:01 +0000

    User Model added

---
+++ server/models/User.js
@@ -0,0 +1,20 @@
+const mongoose=require('mongoose');
+const UserSchema = new mongoose.Schema({
+    id: { type: mongoose.Schema.Types.ObjectId, auto: true },
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  }, { collection: 'Users' });
+  UserSchema.pre('save', async function (next) {
+    try {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(this.password, salt);
+      this.password = hashedPassword;
+      next();
+    } catch (error) {
+      next(error);
+    }
+  });
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
+  
